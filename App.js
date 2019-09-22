67ac759b583c522ee6a7d2f87fba951e3d44c11f
Sithija Shehara <nsithijashehara@gmail.com>
2019-09-22 20:20:03 +0530

    added main screen

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+
+import MainScreen from './components/Main';
 
 export default class App extends Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>To Do</Text>
-      </View>
+      <MainScreen/>
     );
   }
 }
