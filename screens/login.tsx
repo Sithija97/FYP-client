8abd071abbcb4f3f76252f8066c1be5e316037a3
Sithija Shehara <nsithijashehara@gmail.com>
2023-12-10 21:43:29 +0530

    login wireup - component function

--- screens/login.tsx
+++ screens/login.tsx
@@ -1,7 +1,20 @@
 import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
 import { Button, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { useAppDispatch } from "../store";
+import { login } from "../store/auth";
 
 export const Login = () => {
+  const dispatch = useAppDispatch();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    await dispatch(login({ email, password }));
+    setEmail("");
+    setPassword("");
+  };
+
   return (
     <View className="flex-1 items-center justify-center bg-white px-8">
       <StatusBar style="auto" />
@@ -11,15 +24,21 @@ export const Login = () => {
           placeholder="Enter email"
           textContentType="emailAddress"
           keyboardType="email-address"
+          onChangeText={(text) => setEmail(text)}
+          value={email}
         />
         <TextInput
           className="rounded-md p-1 mb-3 border border-slate-200"
           placeholder="Enter password"
           textContentType="password"
           secureTextEntry={true}
+          onChangeText={(text) => setPassword(text)}
+          value={password}
         />
         <TouchableOpacity className="bg-blue-400 p-2 rounded-md ">
-          <Text className="text-white text-center">Login</Text>
+          <Text className="text-white text-center" onPress={handleLogin}>
+            Login
+          </Text>
         </TouchableOpacity>
       </View>
     </View>
